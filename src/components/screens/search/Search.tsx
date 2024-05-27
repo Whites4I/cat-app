a930f0a44125c509c8dee21cd85713c20db320c2
Andriy Petryna <andriypetrina2000@gmail.com>
2024-05-27 20:39:04 +0300

    fix: changed cart for image cats

--- src/components/screens/search/Search.tsx
+++ src/components/screens/search/Search.tsx
@@ -1,23 +1,18 @@
 import { FC, useContext } from 'react'
-import { useAppDispatch } from '../../../hooks/useAppDispatch'
 import { useAppSelector } from '../../../hooks/useAppSelector'
 import { useDivideBlock } from '../../../hooks/useDivideBlock'
 import useSearchByBreed from '../../../hooks/useSearchByBreed'
 import { SearchContext } from '../../layout/Layout'
 import BackBtn from '../../ui/buttons/back-btn/BackBtn'
-import FavoritesToggleBtn from '../../ui/buttons/favorites-toggle-btn/FavoritesToggleBtn'
 import GridCarts from '../../ui/carts/grid-carts/GridCarts'
+import InfoBreed from '../../ui/tables/info-breed/InfoBreed'
 import InfoPage from '../../ui/tables/info-page/InfoPage'
 import styles from './Search.module.scss'
 
 const Search: FC = () => {
-	const { favorites, dislikes, likes } = useAppSelector(
-		state => state.toggleCat
-	)
+	const { dislikes, likes } = useAppSelector(state => state.toggleCat)
 
 	const { breedSearch } = useContext(SearchContext)
-
-	const { toggleFavorites } = useAppDispatch()
 
 	const mergedData = dislikes.concat(likes)
 	const searchData = useSearchByBreed(mergedData, breedSearch as string)
@@ -41,12 +36,7 @@ const Search: FC = () => {
 			</div>
 
 			<div className={styles.cartsSection}>
-				<GridCarts
-					data={searchDivide}
-					dataComponent={favorites}
-					toggle={toggleFavorites}
-					component={FavoritesToggleBtn}
-				/>
+				<GridCarts data={searchDivide} component={InfoBreed} />
 			</div>
 		</div>
 	)
